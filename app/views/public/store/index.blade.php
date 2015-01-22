323fa88d0a1119ba765529180ffd55b8934a5c44
cuongnb14 <cuongnb14@gmail.com>
2015-01-22 18:36:24 +0700

    Giao diện

---
+++ app/views/public/store/index.blade.php
@@ -0,0 +1,137 @@
+@extends("public.layout.main_layout")
+@section('content')
+
+<div class="content-container container-fluid">
+	<div class="col-md-3 sidebar">
+		<div class="panel panel-default">
+			<div class="panel-heading">Dịch vụ</div>
+			<div class="panel-body">
+				<ul class="list-unstyled">
+					<li><label> <input type="checkbox"> Check me
+							out
+					</label></li>
+
+					<li><label> <input type="checkbox"> Check me
+							out
+					</label></li>
+
+					<li><label> <input type="checkbox"> Check me
+							out
+					</label></li>
+
+					<li><label> <input type="checkbox"> Check me
+							out
+					</label></li>
+				</ul>
+			</div>
+		</div>
+
+
+		<div class="panel panel-default">
+			<div class="panel-heading">Dịch vụ</div>
+			<div class="panel-body">
+				<ul class="list-unstyled">
+					<li><label> <input type="checkbox"> Check me
+							out
+					</label></li>
+
+					<li><label> <input type="checkbox"> Check me
+							out
+					</label></li>
+
+					<li><label> <input type="checkbox"> Check me
+							out
+					</label></li>
+
+					<li><label> <input type="checkbox"> Check me
+							out
+					</label></li>
+				</ul>
+			</div>
+		</div>
+
+	</div>
+	<div class="col-md-9 main-content">
+		<div class="box">
+			<h3 class="box-title">Danh sách quán</h3>
+			<div class="box-body">
+
+				<div class="place col-md-6">
+					<div class="thumnail col-md-4">
+						<img src="images/20.jpg" width="100%" height="100%">
+					</div>
+					<div class="description col-md-8">
+						<h3 class="name-place">
+							Cafe Hoa Sữa <a href="#"><i class="fa fa-map-marker"></i></a>
+						</h3>
+						<p class="address">3f Tăng Bạt Hổ, Tp. Đà Lạt, Lâm Đồng, Việt
+							Nam</p>
+						<p class="info">Ai đó đã từng nói “Người Mỹ không cần một sản
+							phẩm khác, người Mỹ cần một câu chuyện khác”.</p>
+					</div>
+				</div>
+
+				<div class="place col-md-6">
+					<div class="thumnail col-md-4">
+						<img src="images/20.jpg" width="100%" height="100%">
+					</div>
+					<div class="description col-md-8">
+						<h3 class="name-place">
+							Cafe Hoa Sữa <a href="#"><i class="fa fa-map-marker"></i></a>
+						</h3>
+						<p class="address">3f Tăng Bạt Hổ, Tp. Đà Lạt, Lâm Đồng, Việt
+							Nam</p>
+						<p class="info">Ai đó đã từng nói “Người Mỹ không cần một sản
+							phẩm khác, người Mỹ cần một câu chuyện khác”.</p>
+					</div>
+				</div>
+
+				<div class="place col-md-6">
+					<div class="thumnail col-md-4">
+						<img src="images/20.jpg" width="100%" height="100%">
+					</div>
+					<div class="description col-md-8">
+						<h3 class="name-place">
+							Cafe Hoa Sữa <a href="#"><i class="fa fa-map-marker"></i></a>
+						</h3>
+						<p class="address">3f Tăng Bạt Hổ, Tp. Đà Lạt, Lâm Đồng, Việt
+							Nam</p>
+						<p class="info">Ai đó đã từng nói “Người Mỹ không cần một sản
+							phẩm khác, người Mỹ cần một câu chuyện khác”.</p>
+					</div>
+				</div>
+
+				<div class="place col-md-6">
+					<div class="thumnail col-md-4">
+						<img src="images/20.jpg" width="100%" height="100%">
+					</div>
+					<div class="description col-md-8">
+						<h3 class="name-place">
+							Cafe Hoa Sữa <a href="#"><i class="fa fa-map-marker"></i></a>
+						</h3>
+						<p class="address">3f Tăng Bạt Hổ, Tp. Đà Lạt, Lâm Đồng, Việt
+							Nam</p>
+						<p class="info">Ai đó đã từng nói “Người Mỹ không cần một sản
+							phẩm khác, người Mỹ cần một câu chuyện khác”.</p>
+					</div>
+				</div>
+
+
+
+
+
+
+
+
+
+
+
+			</div>
+		</div>
+	</div>
+</div>
+
+
+</div>
+<!-- End .content-container -->
+@stop
