f9fd09b907da5c047a982b734dce1938c7d4616a
cuongnb14 <cuongnb14@gmail.com>
2015-02-08 19:34:02 +0700

    hh

--- app/views/public/store/index.blade.php
+++ app/views/public/store/index.blade.php
@@ -62,7 +62,7 @@
 					</div>
 					<div class="description col-md-8">
 						<h3 class="name-place">
-							<a class="link_name" href="{{URL::route('quan_cafe',$place->id)}}">{{$place->name}}</a> <a href="#" class="link_map"><i class="fa fa-map-marker"></i></a>
+							<a class="link_name" href="{{URL::route('quan_cafe',$place->id)}}">{{$place->name}}</a> <a href="{{URL::route('quan_cafe_map',array($place->id))}}" class="link_map"><i class="fa fa-map-marker"></i></a>
 						</h3>
 						<p class="address">3f Tăng Bạt Hổ, Tp. Đà Lạt, Lâm Đồng, Việt
 							Nam</p>
